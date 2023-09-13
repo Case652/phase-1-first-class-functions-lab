7fdb25eef1adfbf2ae8a15c457f7e9f092417a16
Mateusz M Trybunia <Mateusz.m.trybunia@gmail.com>
2023-09-12 19:48:03 -0500

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(drivers) {
+    return drivers.slice(0,2);
+}
+
+function returnLastTwoDrivers(drivers) {
+    return drivers.slice(2,4)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(fareQuintupler) {
+    return function(fair) {
+        return fareQuintupler * fair;
+    }
+}
+const fareDoubler = function(fair) {
+    return createFareMultiplier(2)(fair)
+}
+const fareTripler = function(fair) {
+    return createFareMultiplier(3)(fair)
+}
+function selectDifferentDrivers(drivers, call) {
+    return call(drivers)
+}
